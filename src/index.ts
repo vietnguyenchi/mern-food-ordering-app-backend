132d52d3e106c9981bb2506279c9630f05f749e3
Chi Viet <chiviet296@gmail.com>
2024-06-09 16:48:12 +0700

    database setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose
+	.connect(process.env.MONGODB_CONNECTION_STRING as string)
+	.then((): void => console.log('Connected to database'));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', async (req: Request, res: Response) => {
+app.get('/test', async (req: Request, res: Response): Promise<void> => {
 	res.json({ message: 'Hello!' });
 });
 
-app.listen(7000, () => console.log('Server is running on port 7000'));
+app.listen(7000, (): void => console.log('Server is running on port 7000'));

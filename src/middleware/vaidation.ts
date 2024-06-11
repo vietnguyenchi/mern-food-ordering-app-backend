cc22f7635a5425ad737a68e063872851e97bd22b
Chi Viet <chiviet296@gmail.com>
2024-06-11 14:43:57 +0700

    Create restaurant API

--- src/middleware/vaidation.ts
+++ src/middleware/vaidation.ts
@@ -26,3 +26,37 @@ export const validateMyUserRequest = [
 	body('city').isString().notEmpty().withMessage('City must be a string'),
 	handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+	body('restaurantName')
+		.isString()
+		.notEmpty()
+		.withMessage('Restaurant name must be a string'),
+	body('city').isString().notEmpty().withMessage('City must be a string'),
+	body('country')
+		.isString()
+		.notEmpty()
+		.withMessage('Country must be a string'),
+	body('deliveryPrice')
+		.isFloat()
+		.notEmpty()
+		.withMessage('Delivery price must be a positive number'),
+	body('estimatedDeliveryTime')
+		.isInt()
+		.notEmpty()
+		.withMessage('Estimated delivery time must be a positive integer'),
+	body('cuisines')
+		.isArray()
+		.withMessage('Cuisines must be an array')
+		.not()
+		.isEmpty()
+		.withMessage('Cuisines array cannot be empty'),
+	body('menuItems').isArray().withMessage('Menu items must be an array'),
+	body('menuItems.*.name')
+		.notEmpty()
+		.withMessage('Menu item name is required'),
+	body('menuItems.*.price')
+		.isFloat({ min: 0 })
+		.withMessage('Menu item price is required and must be a positive number'),
+	handleValidationErrors,
+];
---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import multer from 'multer';
+import { createMyRestaurant } from '../controllers/MyRestaurantController';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyRestaurantRequest } from '../middleware/vaidation';
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({
+	storage: storage,
+	limits: {
+		fileSize: 5 * 1024 * 1024,
+	},
+});
+
+router.post(
+	'/',
+	upload.single('imageFile'),
+	validateMyRestaurantRequest,
+	jwtCheck,
+	jwtParse,
+	createMyRestaurant
+);
+
+export default router;

00a3f765369dba92a4aa7e2ba6a9b2f1f9701ba2
Chi Viet <chiviet296@gmail.com>
2024-06-18 23:00:29 +0700

    Fix currency

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -127,7 +127,7 @@ const createLineItems = (
 
 		const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
 			price_data: {
-				currency: 'gbp',
+				currency: 'vnd',
 				product_data: {
 					name: menuItem.name,
 				},
@@ -156,7 +156,7 @@ const createSession = async (
 					type: 'fixed_amount',
 					fixed_amount: {
 						amount: deliveryPrice,
-						currency: 'gbp',
+						currency: 'vnd',
 					},
 				},
 			},
